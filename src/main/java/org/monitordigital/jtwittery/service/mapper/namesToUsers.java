6c473de561bf2ec32b54472f53340d22d47a88fd
gafelix <gabriel.felix@looplex.com.br>
2022-03-21 14:53:42 -0300

    Fix CSV not exporting tweets correctly

--- src/main/java/org/monitordigital/jtwittery/service/mapper/namesToUsers.java
+++ src/main/java/org/monitordigital/jtwittery/service/mapper/namesToUsers.java
@@ -3,11 +3,13 @@ package org.monitordigital.jtwittery.service.mapper;
 import org.monitordigital.jtwittery.model.User;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class namesToUsers {
 
     public static List<User> toUsers(List<String> names) {
+        if(Objects.isNull(names)) return null;
         return names.stream().map(name -> new User(name)).collect(Collectors.toList());
     }
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,5 +6,5 @@ INSERT INTO Tweet(id, text, created_at, author_name, likes, retweets, quotes, re
 
 INSERT INTO Tweet(id, text, created_at, author_name, likes, retweets, quotes, replies, type) VALUES(2, 'RT @gabrielfelix:Eu acho que...', '2007-12-04T10:15:30+01:00', 'pablo_ortellado', 3, 0, 0, 0, 'RETWEET');
 
-INSERT INTO Referenced_Tweet(reference_type, id) VALUES('retweeted', 1);
-INSERT INTO Tweet_Referenced(tweet_id, referenced_reference_type, referenced_id) VALUES(2, 'retweeted', 1);
+INSERT INTO Referenced_Tweet(reference_type, id) VALUES('RETWEETED', 1);
+INSERT INTO Tweet_Referenced(tweet_id, referenced_reference_type, referenced_id) VALUES(2, 'RETWEETED', 1);
