4596e44831278ec871cc98a152142d9123e16978
ga-felix <gafelix435@gmail.com>
2022-03-27 13:42:47 -0300

    Fix mapper bug

--- src/main/java/org/monitordigital/jtwittery/service/mapper/TweetMapper.java
+++ src/main/java/org/monitordigital/jtwittery/service/mapper/TweetMapper.java
@@ -6,6 +6,7 @@ import org.monitordigital.jtwittery.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
@@ -15,6 +16,7 @@ public class TweetMapper {
     private UserRepository userRepository;
 
     public List<User> toUsers(List<String> ids) throws Exception {
+        if(Objects.isNull(ids)) return null;
         return ids
                 .stream()
                 .map(id -> userRepository.findById(Long.valueOf(id)).orElseThrow())
